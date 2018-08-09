610fbb502181674d674118a25f709d4a0e4fd04c
Heikki Pernu <heikki.pernu@nekonet.fi>
2018-08-09 11:08:11 +0300

    Fix more paths in test scripts

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ mkdir -p /tmp/shared-yum-cache
 docker run -v /tmp/shared-yum-cache:/var/cache/yum fmibase yum update -y
 test `find /tmp/shared-yum-cache | wc -l` -gt 0
 docker run -v /tmp/shared-yum-cache:/var/cache/yum -v /tmp/shared-ccache:/ccache fmidev ccache -s
-test `find /tmp/ccache | wc -l` -gt 0
+test `find /tmp/shared-ccache | wc -l` -gt 0
 
 ls -l /tmp/shared-ccache
 ls -l /tmp/shared-yum-cache

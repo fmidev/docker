77c13c49b2e9113bf0bd8894b4cfd2a381f7016d
Heikki Pernu <heikki.pernu@nekonet.fi>
2018-08-09 13:21:59 +0300

    Add actual ccache testing

--- test-inside-container.sh
+++ test-inside-container.sh
@@ -7,4 +7,21 @@ ls -l /var/cache/yum
 test `find /var/cache/yum/ | wc -l` -gt 0
 test `find /ccache/ | wc -l` -gt 0
 
+ccache -s
+
+yum install -y gcc
+
+cat >/tmp/in.c <<EOF
+#include <stdio.h>
+
+int main(int argc,char *argv[]) {
+    printf("arg: %d %s\n",argc,argv[0]);
+    return 0;
+}
+EOF
+
+gcc -Wall -o /tmp/out /tmp/in.c
+amount=`ccache -s | fgrep "files in cache" | sed -e 's/^[^0-9]*//'`
+test "$amount" -gt "0"
+
 
